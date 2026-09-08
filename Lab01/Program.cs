2c165cb546653d2c5f81ed8e6c4436d08a12051d
Кадралиева Аделина <adel.kadralieva@gmail.com>
2026-09-08 15:01:16 +0300

    Lab01: variables and interpolation

---
+++ Lab01/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string myName = "Кадралиева Аделина";
+string groupName = "ИСП-251";
+int courseNumber = 2;
+double averageGrade = 4.8;
+bool isBudget = true;
+
+Console.WriteLine("Знакомство");
+Console.WriteLine($"Студент: {myName}");
+Console.WriteLine($"Группа: {groupName}");
+Console.WriteLine($"Курс: {courseNumber}");
+Console.WriteLine($"Средний балл: {averageGrade}");
+Console.WriteLine($"Бюджетное место: {isBudget}");

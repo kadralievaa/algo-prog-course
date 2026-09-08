b79c9c17f70f11f7668e6f8aed0e0986520e9ea6
Кадралиева Аделина <adel.kadralieva@gmail.com>
2026-09-08 16:55:01 +0300

    x

---
+++ Visitka/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine();
+string myName = "Кадралиева Аделина";
+string group = "ИСП-251";
+int course = 2;
+string sp = "09.02.07";
+
+double ball = 4.2;
+double ball1 = 4.6;
+double ball2 = 4.9;
+const double min_ball = 4.2;
+double srball = (ball + ball1 + ball2) / 3.0;
+
+bool grant = 

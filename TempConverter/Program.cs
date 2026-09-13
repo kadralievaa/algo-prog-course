7576c8e03f28b91b69f5f1aa92a6bd2b48dba3db
Кадралиева Аделина <adel.kadralieva@gmail.com>
2026-09-13 22:28:25 +0300

    Lab01 task 2: temperature converter

---
+++ TempConverter/Program.cs
@@ -0,0 +1,10 @@
+﻿double celsius = 23.5;
+
+const double F_factor = 9.0 / 5.0;
+const double F_offset = 32.0;
+const double K_offest = 273.15;
+
+double fahrenheit = celsius * F_factor + F_offset;
+double kelvin = celsius + K_offest;
+
+Console.WriteLine($"{celsius}°C = {fahrenheit:F2}°F = {kelvin:F2}K");
